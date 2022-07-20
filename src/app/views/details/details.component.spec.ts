5ff91c1abf6e2ebfbb051fb7dfb682d9536d61b9
Pinto Júnior <junior_pinto@hotmail.com>
2022-07-20 18:39:54 -0300

    Remove useless comments and update tests.

--- src/app/views/details/details.component.spec.ts
+++ src/app/views/details/details.component.spec.ts
@@ -1,25 +1,20 @@
-
-import { HttpClient } from '@angular/common/http';
-import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ActivatedRoute } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
-import { async, Observable, of } from 'rxjs';
+import { of } from 'rxjs';
 import { ApiService } from 'src/app/api.service';
 import { AppModule } from 'src/app/app.module';
-
 import { DetailsComponent } from './details.component';
 
 describe('DetailsComponent', () => {
   let component: DetailsComponent;
   let fixture: ComponentFixture<DetailsComponent>;
   let apiService: ApiService;
-  let httpClient: HttpClient;
 
   let mockApiService: jasmine.SpyObj<ApiService>;
-  
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -31,17 +26,6 @@ describe('DetailsComponent', () => {
         AppModule,
       ],
       providers: [
-        // {
-        //   provide: ApiService,
-        //   useValue: {
-        //     getAnimeById: () =>
-        //       of({
-        //         data: {
-        //           attributes: 'One Punch Man',
-        //         },
-        //       }),
-        //   },
-        // },
         {
           provide: ActivatedRoute,
           useValue: { snapshot: { params: { id: '10740' } } },
@@ -51,10 +35,6 @@ describe('DetailsComponent', () => {
   });
 
   beforeEach(() => {
-    
-    // TestBed.configureTestingModule({
-    //   imports: [HttpClientTestingModule, RouterTestingModule],
-    // });
     fixture = TestBed.createComponent(DetailsComponent);
     component = fixture.componentInstance;
     apiService = TestBed.inject(ApiService);
@@ -64,24 +44,6 @@ describe('DetailsComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
-  // it('should create the app', () => {
-  //   apiService = new ApiService(httpClient);
-  //   expect(apiService).toBeTruthy();
-  // });
-
-  // it('should return value for idAnime', () => {
-  //   component.idAnime = '10740';
-  //   expect(typeof(component.idAnime)).toBe('string');
-  // });
-
-  // it('should return value for queryAnime', () => {
-  //   component.idAnime = '10740';
-  //   apiService = new ApiService(httpClient);
-  //   apiService.getAnimeById(component.idAnime).subscribe((response) => {
-  //     component.attributes = response.attributes;
-  //   });
-  //   expect(typeof(component.attributes)).toBe('');
-  // });
 
   it('shold have title', () => {
     component.attributes = {
@@ -89,14 +51,13 @@ describe('DetailsComponent', () => {
       canonicalTitle: 'New title',
     };
     fixture.detectChanges();
-    const elementTitle = fixture.debugElement.query(By.css('.title'))
-    expect((elementTitle.nativeElement as HTMLAnchorElement).textContent).toContain('New title')
-    
-  })
-
+    const elementTitle = fixture.debugElement.query(By.css('.title'));
+    expect(
+      (elementTitle.nativeElement as HTMLAnchorElement).textContent
+    ).toContain('New title');
+  });
 
   it("should return ApiService from getAnimeById as 'One Punch Man'", () => {
-    // fixture.debugElement.injector.get(ApiService);
     spyOn(apiService, 'getAnimeById').and.returnValue(
       of({
         data: {
@@ -108,6 +69,3 @@ describe('DetailsComponent', () => {
     expect(component.attributes).toBe('One Punch Man');
   });
 });
-
-
-
