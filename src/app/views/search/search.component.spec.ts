60b3af05f26ff3c8ee77ef5cb2b136b7e4a4db13
Pinto Júnior <junior_pinto@hotmail.com>
2022-07-20 18:41:29 -0300

    Remove useless comments and update tests.

--- src/app/views/search/search.component.spec.ts
+++ src/app/views/search/search.component.spec.ts
@@ -1,17 +1,52 @@
+import { AppModule } from 'src/app/app.module';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormsModule } from '@angular/forms';
 import { RouterTestingModule } from '@angular/router/testing';
-
+import { of } from 'rxjs';
+import { ApiService } from 'src/app/api.service';
 import { SearchComponent } from './search.component';
 
 describe('SearchComponent', () => {
   let component: SearchComponent;
   let fixture: ComponentFixture<SearchComponent>;
+  let mockApiService: jasmine.SpyObj<ApiService>;
+  const routerSpy = jasmine.createSpyObj('Router', ['navigateByUrl']);
+  const animeMock = {
+    id: '10740',
+    attributes: {
+      canonicalTitle: 'One Punch Man',
+      synopsis: 'string',
+      episodeCount: 1,
+      posterImage: {
+        tiny: 'string',
+        small: 'string',
+        medium: 'string',
+        large: 'string',
+        original: 'string',
+      },
+    },
+  };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule, RouterTestingModule, FormsModule],
+      imports: [
+        HttpClientTestingModule,
+        RouterTestingModule,
+        FormsModule,
+        AppModule,
+      ],
+      providers: [
+        {
+          provide: ApiService,
+          useValue: {
+            getAnimeByTitle: () =>
+              of({
+                data: mockApiService,
+              }),
+          },
+        },
+      ],
       declarations: [SearchComponent],
     }).compileComponents();
   });
@@ -24,5 +59,13 @@ describe('SearchComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call goToDetailsByService function', () => {
+    const spyGo = spyOn(component, 'goToDetailsByService').and.callThrough();
+    const spy = spyOn(component['router'], 'navigateByUrl');
+    component.goToDetailsByService(animeMock);
+    expect(spyGo).toHaveBeenCalled();
+    expect(spy).toHaveBeenCalledWith('/details/edit/' + animeMock.id);
   });
 });
